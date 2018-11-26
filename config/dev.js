c89e63d3c18f58fb91ace51381ffb559f2047810
Rachna <rachnasinghal2005@gmail.com>
2018-11-26 12:44:44 +0530

    deleting dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://rachna:rachna@ds115154.mlab.com:15154/box-drive',
-  secretKey: 'asdsadsadsdtggbgbkhjykhjkhgbgbg',
-  cloudName: 'rachnaban',
-  uploadPreset: 'chingu',
-};
